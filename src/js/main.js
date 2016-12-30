f5b943d5ad7d57c45e0a5ed81c5a10caab45830c
rocklabmusiker <rocklabmusiker@gmail.com>
2016-12-30 18:43:32 +0100

    first

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$('<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic" rel="stylesheet">').appendTo('head');// rest of css assync
+$('<link rel=stylesheet type=text/css href=css/main.css>').appendTo('head');// rest of css assync
+
+//= libs/jquery.lazyload.min.js
+//= libs/jquery.fancybox.pack.js
+//= libs/jquery.arcticmodal.min.js
+//= libs/jquery.maskedinput.min.js
+
+
+
