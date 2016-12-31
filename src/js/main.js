b66783cd91cdabf5db485fbc34bfcd7111188dc7
rocklabmusiker <rocklabmusiker@gmail.com>
2016-12-31 14:03:08 +0100

    header+bitten

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@
 $('<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic" rel="stylesheet">').appendTo('head');
 $('<link rel=stylesheet type=text/css href=css/main.css>').appendTo('head');
 
+//= libs/jquery.flip.min.js
+//= libs/modernizr.js
+//= libs/fusionad.js
 //= libs/app.js
-
 
 
